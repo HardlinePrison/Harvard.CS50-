973d3fdf6da31eb0913cdd0520946dda718c1007
HardlinePrison <47038545+HardlinePrison@users.noreply.github.com>
2024-02-05 15:51:08 +0100

    Update mario.c

--- pset1/Mario/less/mario.c
+++ pset1/Mario/less/mario.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -8,36 +8,36 @@ int main(void)
 
     do
     {
-    int pHeight = get_int("Please enter pyramide height between 0 and 23: ");
-    int lenght = pHeight++;
-    int amountHash = 2;
+        int pHeight = get_int("Please enter pyramide height between 1 and 8: ");
+        int lenght = pHeight;
+        int amountHash = 1;
 
-
-
-            if(pHeight > 0 && pHeight < 25)
+        if (pHeight >= 1 && pHeight <= 8)
+        {
+            inputRight = false;
+            for (int i = 0; i < pHeight; i++)
             {
-                inputRight = false;
-                for(int i = 1; i < pHeight; i++)
+                for (int in = 1; in < lenght; in++)
                 {
-                    for(int in = 1; in < lenght; in++)
-                    {
-                        printf(" ");
-                    }
-
-                    for(int it = 0; it < amountHash; it++)
-                    {
-                        printf("#");
-                    }
-
-                    amountHash++;
-                    lenght--;
-                    printf("\n");
+                    printf(" ");
                 }
-            }
-            else
-            {
-                printf("input is not correct please try again\n");
-            }
-    } while(inputRight);
-}
 
+                for (int it = 0; it < amountHash; it++)
+                {
+                    printf("#");
+                }
+
+                amountHash++;
+                lenght--;
+                printf("\n");
+            }
+        }
+        else
+        {
+            printf("input is not correct please try again\n");
+            inputRight = true;
+        }
+    }
+    while (inputRight);
+    return 0;
+}
